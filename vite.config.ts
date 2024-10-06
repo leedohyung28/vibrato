bf53eaf4aa472a7795f0ddcb43e01d5661042878
Bhangdongho <bhang97@naver.com>
2024-10-07 01:28:08 +0900

    :zap: versel 배포 관련 파일 추가 및 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {},
-    }),
-  ],
-  define: {
-    VITE_SPOTIFY_CLIENT_ID: JSON.stringify(" f02dbdc5d74f4b9aa10fcc00ae912943"),
-    VITE_SPOTIFY_CLIENT_SECRET: JSON.stringify(
-      "de85e1845047418ab6811a2eb034afc0"
-    ),
-    VITE_SPOTIFY_REDIRECT_URI: JSON.stringify("http://localhost:5173/callback"),
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {},
+      }),
+    ],
+    base: mode === "production" ? "/your-base-url/" : "/",
+    define: {
+      VITE_SPOTIFY_CLIENT_ID: JSON.stringify(
+        "f02dbdc5d74f4b9aa10fcc00ae912943"
+      ),
+      VITE_SPOTIFY_CLIENT_SECRET: JSON.stringify(
+        "de85e1845047418ab6811a2eb034afc0"
+      ),
+      VITE_SPOTIFY_REDIRECT_URI: JSON.stringify(
+        "http://localhost:5173/callback"
+      ),
+      VITE_API_BASE_URL: JSON.stringify(process.env.VITE_API_BASE_URL || ""),
+    },
+  };
 });
