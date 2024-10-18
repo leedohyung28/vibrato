e42bf89fe223b6a0233b35e9cc44febb186cc929
Bhangdongho <bhang97@naver.com>
2024-10-18 09:36:03 +0900

    주소 변경

--- src/apis/track.ts
+++ src/apis/track.ts
@@ -4,9 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getTrackInfo = async (trackId: string) => {
   try {
-    const response = await axios.put(
-      `${API_BASE_URL}/search/single/track/${trackId}`
-    );
+    const response = await axios.put(`/search/single/track/${trackId}`);
     return response.data;
   } catch (error) {
     console.error("트랙 정보를 불러오지 못했습니다:", error);
